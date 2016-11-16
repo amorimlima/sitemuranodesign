d6c4de8e9f9fe7ef1f07141714231f769261661b
Letícia Brito <leticia.brito@muranodesign.com.br>
2016-11-16 13:43:07 -0200

    Capa Mobile

--- assets/includes/video.php
+++ assets/includes/video.php
@@ -1,8 +1,7 @@
 <section id="Capa">
 
-<div class="fullscreen-bg">
+<div class="headerMobile">
 <video src="videos/video_sitemurano.mp4" type="video/mp4" poster="images/videogif.gif" id="bgvid" playsinline autoplay muted loop>
-  <!-- WCAG general accessibility recommendation is that media such as background video play through only once. Loop turned on for the purposes of illustration; if removed, the end of the video will fade in the same way created by pressing the "Pause" button  -->
 </video>
 </div>
 
